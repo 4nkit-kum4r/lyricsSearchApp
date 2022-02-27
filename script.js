19cf5740ce981ab8981618f639d83e2d2f23de82
4nkit-kum4r <99010258+4nkit-kum4r@users.noreply.github.com>
2022-02-28 01:12:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
-const form = document.getElementById('form');
-const search = document.getElementById('search');
-const result = document.getElementById('result');
-const more = document.getElementById('more');
-
 const apiURL = 'https://api.lyrics.ovh';
 
-// Search by song or artist
 async function searchSongs(term) {
     const res = await fetch(`${apiURL}/suggest/${term}`);
     const data = await res.json();
@@ -13,46 +7,19 @@ async function searchSongs(term) {
     showData(data);
 }
 
-// Show song and artist in DOM
+const result = document.getElementById('result');
+
 function showData(data) {
     result.innerHTML = `
     <ul class="songs">
-      ${data.data
-            .map(
-                song => `<li>
+      ${data.data.map(song => `<li>
       <span><strong>${song.artist.name}</strong> - ${song.title}</span>
       <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
-    </li>`
-            )
-            .join('')}
+      </li>`).join('')}
     </ul>
   `;
-
-    if (data.prev || data.next) {
-        more.innerHTML = `
-      ${data.prev
-                ? `<button class="btn" onclick="getMoreSongs('${data.prev}')">Prev</button>`
-                : ''
-            }
-      ${data.next
-                ? `<button class="btn" onclick="getMoreSongs('${data.next}')">Next</button>`
-                : ''
-            }
-    `;
-    } else {
-        more.innerHTML = '';
-    }
 }
 
-// Get prev and next songs
-async function getMoreSongs(url) {
-    const res = await fetch(`https://cors-anywhere.herokuapp.com/${url}`);
-    const data = await res.json();
-
-    showData(data);
-}
-
-// Get lyrics for song
 async function getLyrics(artist, songTitle) {
     const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
     const data = await res.json();
@@ -67,24 +34,19 @@ async function getLyrics(artist, songTitle) {
             <span>${lyrics}</span>
         `;
     }
-
-    more.innerHTML = '';
 }
 
-// Event listeners
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+
 form.addEventListener('submit', e => {
     e.preventDefault();
 
     const searchTerm = search.value.trim();
 
-    if (!searchTerm) {
-        alert('Please type in a search term');
-    } else {
-        searchSongs(searchTerm);
-    }
+    searchTerm ? searchSongs(searchTerm) : alert('Please type in a search term');
 });
 
-// Get lyrics button click
 result.addEventListener('click', e => {
     const clickedEl = e.target;
 
